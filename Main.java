a9718ad53210216397b2b034d2df5dc636859880
owokaiser <101186844+owokaiser@users.noreply.github.com>
2022-03-22 11:40:35 +0000

    final commit

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 class Main {
   public static void main(String[] args) {
     int x;
+    char y;
+    long z;
+    double v; 
     x=5;
-    
-    System.out.println("Wartość zmienniej to: "+x);
+    y='a' ;
+    z=123;
+    v=1.23;
+    System.out.println("Wartość zmienniej 1 to: "+x);
+    System.out.println("Wartość zmienniej 2 to: "+y);
+    System.out.println("Wartość zmienniej 3 to: "+z);
+    System.out.println("Wartość zmienniej 4 to: "+v);
   }
 }
